f34a735a329c3705b768892a46d10434188b7bc4
Eduardo R. Golding <eduardo.golding@ubeeqo.com>
2021-04-18 11:41:43 +0200

    Changed mapstruct for snout tag

--- bootstrap.go
+++ bootstrap.go
@@ -23,7 +23,6 @@ type Kernel struct {
 type Options func(kernel *Kernel)
 
 func (k *Kernel) Bootstrap(name string, cfg interface{}, opts ...Options) kernelBootstrap {
-
 	ctx := k.signalling()
 
 	k.varFetching(name, cfg)
@@ -38,18 +37,24 @@ func (k *Kernel) Bootstrap(name string, cfg interface{}, opts ...Options) kernel
 func (k Kernel) varFetching(name string, cfg interface{}) {
 	viper.AutomaticEnv()
 	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
+
 	flagSet := pflag.NewFlagSet(name, pflag.ContinueOnError)
-	if err := gpflag.ParseTo(cfg, flagSet, sflags.FlagDivider("."), sflags.FlagTag("mapstructure")); err != nil {
+
+	if err := gpflag.ParseTo(cfg, flagSet, sflags.FlagDivider("."), sflags.FlagTag("snout")); err != nil {
 		panic(err)
 	}
+
 	if err := viper.BindPFlags(flagSet); err != nil {
 		panic(err)
 	}
+
 	viper.SetConfigName(name)
 	viper.AddConfigPath(".")
+
 	if err := viper.ReadInConfig(); err == nil {
 		fmt.Printf("Using config file: %s \n", viper.ConfigFileUsed())
 	}
+
 	if err := viper.Unmarshal(cfg); err != nil {
 		panic(err)
 	}
@@ -58,19 +63,22 @@ func (k Kernel) varFetching(name string, cfg interface{}) {
 func (k Kernel) signalling() context.Context {
 	ctx, cancel := context.WithCancel(context.Background())
 	ch := make(chan os.Signal, 1)
+
 	signal.Notify(ch, syscall.SIGTERM, syscall.SIGINT)
+
 	go func() {
 		<-ch
 		signal.Stop(ch)
 		cancel()
 	}()
+
 	return ctx
 }
 
 type kernelBootstrap struct {
 	context context.Context
-	cfg  interface{}
-	runE interface{}
+	cfg     interface{}
+	runE    interface{}
 }
 
 func (kb kernelBootstrap) Initialize() error {
@@ -78,11 +86,13 @@ func (kb kernelBootstrap) Initialize() error {
 	if typeOf.Kind() != reflect.Func {
 		return fmt.Errorf("%s is not a reflect.Func", reflect.TypeOf(kb.runE))
 	}
+
 	var In []reflect.Value
 	In = append(In, reflect.ValueOf(kb.context))
 	In = append(In, reflect.ValueOf(kb.cfg).Elem())
 
 	call := reflect.ValueOf(kb.runE).Call(In)
+
 	err, ok := call[0].Interface().(error)
 	if !ok {
 		return nil
--- example/main.go
+++ example/main.go
@@ -12,26 +12,28 @@ func main() {
 	kernel := snout.Kernel{
 		RunE: Run,
 	}
+
 	kernelBootstrap := kernel.Bootstrap(
 		"agent",
 		&Config{},
 	)
+
 	if err := kernelBootstrap.Initialize(); err != nil {
-		if err != context.Canceled {
+		if !errors.Is(err, context.Canceled) {
 			panic(err)
 		}
 	}
 }
 
 type Config struct {
-	Kafka struct {
-		BrokerAddress string `mapstructure:"broker_address"`
-		ConsumerGroup string `mapstructure:"consumer_group"`
-		Topic         string `mapstructure:"topic"`
-	} `mapstructure:"kafka"`
 	App struct {
-		//...
-	} `mapstructure:"app"`
+		// ...
+	} `snout:"app"`
+	Kafka struct {
+		BrokerAddress string `snout:"broker_address"`
+		ConsumerGroup string `snout:"consumer_group"`
+		Topic         string `snout:"topic"`
+	} `snout:"kafka"`
 }
 
 func Run(ctx context.Context, cfg Config) error {
